1d3c2fea88fa2da45ef0de977c35a6ad5086f3a0
syo <syo@syonoMacBook-puro.local>
2020-08-04 23:33:05 +0900

    htmlファイルを返すように修正

--- src/main/scala/Modoki01.scala
+++ src/main/scala/Modoki01.scala
@@ -1,10 +1,12 @@
-import java.io.{InputStream, OutputStream}
-import java.net.ServerSocket
+import java.io.{FileInputStream, InputStream, OutputStream}
+import java.net.{ServerSocket, Socket}
 import java.text.SimpleDateFormat
 import java.util.{Calendar, Locale, TimeZone}
 
+import scala.util.{Failure, Success, Try}
+
 object Modoki01 extends App {
-  private val DOCUMENT_ROOT = "./"
+  private val DOCUMENT_ROOT = "./public"
 
   val server = new ServerSocket(9999)
   val socket = server.accept()
@@ -12,10 +14,30 @@ object Modoki01 extends App {
   val input = socket.getInputStream()
 
 
-  val line = readLine(input)
+  val path = getPath(input)
+  println(path)
 
-  println(line)
+  val output = socket.getOutputStream()
 
+  // レスポンスヘッダを返す
+  writeLine(output, "HTTP/1.1 200 OK")
+  writeLine(output, "Date: " + getDataStringUtc())
+  writeLine(output, "Server: Modoki/0.1")
+  writeLine(output, "Connection: close")
+  writeLine(output, "Content-type: text/html")
+  writeLine(output, "")
+
+  // レスポンスボディを返す
+  try {
+    val fis = new FileInputStream(DOCUMENT_ROOT + path)
+    val content = readFile(fis)
+    println(content)
+    content.foreach(ch => output.write(ch))
+
+    socket.close()
+  } catch {
+    case e: Exception => println(e)
+  }
 
   // Requestからpathを取得する
   def getPath(input: InputStream): String = readLine(input) match {
@@ -42,12 +64,18 @@ object Modoki01 extends App {
 
   // 1行の文字列を、バイト列としてOutputStreamに書き込む
   def writeLine(output: OutputStream, line: String): Unit = {
-    line match {
-      case "" => ()
-      case _ =>
-        output.write(line.head)
-        writeLine(output, line.tail)
+    line.toCharArray.toList
+    def pWriteLine(output: OutputStream, bytes: List[Char]): Unit = {
+      bytes match {
+        case Nil =>
+          output.write('\r')
+          output.write('\n')
+        case x::xs =>
+          output.write(x)
+          pWriteLine(output, xs)
+      }
     }
+    pWriteLine(output, line.toCharArray.toList)
   }
 
   // 現在時刻から、HTTP標準に合わせてフォーマットされた日付文字列を返す
@@ -56,6 +84,15 @@ object Modoki01 extends App {
     val df = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss", Locale.US)
 
     df.setTimeZone(cal.getTimeZone())
-    df.format(cal.getTime() + " GMT")
+    df.format(cal.getTime()) + " GMT"
+  }
+
+  // ファイルの内容を読み込む
+  def readFile(input: FileInputStream): Array[Int] = {
+    def pReadFile(input: FileInputStream, content: Array[Int]): Array[Int] = input.read() match {
+      case -1 => content
+      case ch => pReadFile(input, content ++ Array(ch))
+    }
+    pReadFile(input, Array())
   }
 }
