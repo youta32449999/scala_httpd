3fb844dc64026cc0ecbb7963825fcaf2e9d2ddb9
s_mori <s_mori@aidma-hd.jp>
2020-08-05 18:01:08 +0900

    Actorの負荷分散を追加

--- src/main/scala/Eseche.scala
+++ src/main/scala/Eseche.scala
@@ -4,15 +4,15 @@ import java.text.SimpleDateFormat
 import java.util.{Calendar, Locale, TimeZone}
 
 import akka.actor.{ActorSystem, Props}
-
 import ServerActorMessage.Run
+import akka.routing.RoundRobinPool
 
 object Eseche extends App {
   private val DOCUMENT_ROOT = "./public"
 
   val server = new ServerSocket(9999)
   val system = ActorSystem("system")
-  val serverActor = system.actorOf(Props[ServerActor], "serverActor")
+  val serverActor = system.actorOf(RoundRobinPool(5).props(Props[ServerActor]), "serverActorPool")
 
   while(true){
     val socket = server.accept()
