22290b9dee1a1b1ee2ccbccfb5acf4c2d9375447
s_mori <s_mori@aidma-hd.jp>
2020-08-05 18:24:01 +0900

    不要な記述を削除

--- src/main/scala/Eseche.scala
+++ src/main/scala/Eseche.scala
@@ -1,15 +1,10 @@
-import java.io.{FileInputStream, InputStream, OutputStream}
-import java.net.{ServerSocket, Socket}
-import java.text.SimpleDateFormat
-import java.util.{Calendar, Locale, TimeZone}
-
+import java.net.{ServerSocket}
 import akka.actor.{ActorSystem, Props}
-import ServerActorMessage.Run
 import akka.routing.RoundRobinPool
 
-object Eseche extends App {
-  private val DOCUMENT_ROOT = "./public"
+import ServerActorMessage.Run
 
+object Eseche extends App {
   val server = new ServerSocket(9999)
   val system = ActorSystem("system")
   val serverActor = system.actorOf(RoundRobinPool(5).props(Props[ServerActor]), "serverActorPool")
